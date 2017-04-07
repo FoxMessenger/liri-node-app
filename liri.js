c24aa3adb3bd08ede62d6863d9f4f04975147784
foxmessenger <iamliamfox@gmail.com>
2017-04-06 19:46:47 -0500

    program is completely functiional

--- liri.js
+++ liri.js
@@ -10,20 +10,30 @@ var inquirer = require('inquirer');
 // SWITCH CASE 1: pull a series of tweets from my personal account
 // ==============
 var myTweets = {
-	check: "Twitter Check", // use this just to make sure this is working
+	check: 'Twitter Check', // use this just to make sure this is working
 	// main twitter pull function
 	tweets: function() {
 		// grabbing from the twitter NPM, as opposed to the twitter keys I have stored
 		var twitter = require('twitter');
 		var keys = new twitter(twitterKeys.twitterKeys);
-		var params = {screen_name: 'mianmagugeti'};
+		var params = [
+			{screen_name: 'mianmagugeti'},
+			{count: 20}
+		];
+		// var count = 20;
 		keys.get('statuses/user_timeline', params, function(error, tweets, response) {
   			if (!error) {
-  				// running a for loop to get the first 10 tweets (+1 because the array starts at 0, and I wanted "i" to start at 1)
-  				for (i = 1; i < 22; i++) {
+  				// running a for loop to get the first 10 tweets (+1 because the array starts at 0, and I wanted 'i' to start at 1)
+  				for (i = 0; i < 20; i++) {
   				// print to the screen the information. "\n" creates a line break
-  				console.log("\n" + i + ": " + tweets[i].text + "\n");
-  				} // --- END for loop
+  				// console.log(tweets.id);
+
+  					console.log(JSON.stringify(tweets[i].text, null, 2) + '\n');
+  					
+  				}
+
+  				// console.log('\n' + i + ': ' + tweets[i].text + '\n');
+  				// } // --- END for loop
   			} // --- END if not err
 		}) // --- END keys.get
 	} // --- END tweets function()
@@ -41,9 +51,9 @@ var spotifyThisSong = {
 	prompt: function() {
 		inquirer.prompt([
 			{
-				type: "input",
-				message: "Type in a song: ",
-				name: "song"
+				type: 'input',
+				message: 'Type in a song: ',
+				name: 'song'
 			} // --- END inquire.prompt
 		]).then( function(user) {
 			// so the spotify API has VERY LITTLE documentation. I'm not even sure I understood everything.
@@ -51,7 +61,7 @@ var spotifyThisSong = {
 			var spotify = require('spotify');
 
 			// take the input of, in the case you get nothing you will default as Ace of Base
-			var userSong = user.song || "Ace of Base";
+			var userSong = user.song || 'Ace of Base';
 			// now, the actual search.
 			spotify.search({ type: 'track', query: ('"' + userSong + '"')}, function(err, data) {
 			    if ( err ) {
@@ -73,6 +83,34 @@ var spotifyThisSong = {
 			}); // --- END songSearch function
 		}) // --- END promise
 	}, // --- END spotifyThisSong.prompt	
+
+	justRequest: function(randomTxt) {
+			// so the spotify API has VERY LITTLE documentation. I'm not even sure I understood everything.
+			// once a song is input we want to run the function songSearch
+			var spotify = require('spotify');
+
+			// take the input of, in the case you get nothing you will default as Ace of Base
+			var userSong = randomTxt || 'Ace of Base';
+			// now, the actual search.
+			spotify.search({ type: 'track', query: ('"' + randomTxt + '"')}, function(err, data) {
+			    if ( err ) {
+			        console.log('Error occurred: ' + err);
+			        return;
+			    }	
+			    // console.log(data)
+			   	// this is the longest set of arrays and objects I've ever had to dig through.
+			    var name = data.tracks.items[1].artists[0].name;
+			    var songName = data.tracks.items[0].name;
+			    var previewLink = data.tracks.items[0].external_urls.spotify;
+			    var album = data.tracks.items[0].album.name 
+
+		    	console.log(JSON.stringify('artist/band: ' + name));
+			    console.log(JSON.stringify('song title: ' + songName));
+			    console.log(JSON.stringify('album title ' + album));
+			    console.log(JSON.stringify('preview link: ' + previewLink));
+
+			}); // --- END songSearch function
+	}, // --- END spotifyThisSong.justRequest
 } // --- END spotifyThisSong object
 
 
@@ -83,62 +121,66 @@ var spotifyThisSong = {
 // ==============
 var movieThis = {	
 	
-	check: console.log("give me a movie."),
+	check: console.log('give me a movie.'),
 	prompt: function() {
 
 		inquirer.prompt([
 			{
-				type: "input",
-				message: "Type in a movie: ",
-				name: "movie"
+				type: 'input',
+				message: 'Type in a movie: ',
+				name: 'movie'
 			} // --- END inquire.prompt
 		]).then(function(user) {
 
 			var request = require('request');
 			request('http://www.omdbapi.com/?t=' + user.movie, function (error, response, body) {
-				console.log("http://www.omdb.com/?t=" + user.movie)
+				console.log('http://www.omdb.com/?t=' + user.movie)
   				console.log('error:', error); // Print the error if one occurred 
-  				console.log("response: " + JSON.stringify(response)); // && response.statusCode // Print the response status code if a response was received 
-  				console.log("body: " + JSON.stringify(body));
+  				console.log('response: ' + JSON.stringify(response)); // && response.statusCode // Print the response status code if a response was received 
+  				console.log('body: ' + JSON.stringify(body));
 			});
 		});
-	}
+	},
 };
 
 // ==============
 // SWITCH CASE 4: use the broken Spotify NPM to choose SONG
 // ==============
 var doWhatItSays = function (){
-	console.log("what does the fox say?");
+	var fs = require('fs')
+
+	fs.readFile("random.txt", "utf8", function(error, data) {
+  		spotifyThisSong.justRequest(data);
+	});
 };
 
 inquirer.prompt([
 	{
-		type: "list",
-		message: "Choose an option:",
-		choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
-		name: "options"
+		type: 'list',
+		message: 'Choose an option:',
+		choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
+		name: 'options'
 		
 	}
 ]).then(function(user) {
 	
-	console.log("choice is: " + user.options);
+	console.log('choice is: ' + user.options);
 	
 	switch(user.options) {
-		case "my-tweets":
+		case 'my-tweets':
 		// run the function tweets of myTweets
 		myTweets.tweets();
 		break;
 
-		case "spotify-this-song":
+		case 'spotify-this-song':
 		spotifyThisSong.prompt();
 		break;
 
-		case "movie-this":
+		case 'movie-this':
 		movieThis.prompt();
 		break;
 
-		case "do-what-it-says":
+		case 'do-what-it-says':
 		doWhatItSays();
 		break;
 	}
